0b9018f8491c9a292e9a7dbded31c6419f880b54
Cristian Rodriguez <crorodriguezro@gmail.com>
2017-12-05 00:20:20 -0500

    validacion

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import io.ParseFile;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
